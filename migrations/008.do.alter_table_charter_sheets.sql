96781f899a019d6ef1024ed48146989ee0334e46
Gage <gagemigan@gmail.com>
2020-07-28 09:04:39 -0400

    THE BIG SEED is done... but found another error lol ...sigh

--- migrations/008.do.alter_table_charter_sheets.sql
+++ migrations/008.do.alter_table_charter_sheets.sql
@@ -1,2 +1,20 @@
 ALTER TABLE "character_sheet" 
-ALTER COLUMN attack_and_spellcasting_info TEXT NULL;
+ALTER COLUMN attack_and_spellcasting_info TEXT NULL,
+ADD COLUMN acrobatics_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN animal_handling_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN arcana_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN athletics_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN deception_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN history_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN insight_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN intimidation_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN investigation_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN medicine_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN nature_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN perception_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN performance_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN persuasion_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN religon_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN sleight_of_hand_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN stealth_expertise BOOLEAN DEFAULT false NOT NULL,
+ADD COLUMN survival_expertise BOOLEAN DEFAULT false NOT NULL;
--- migrations/008.undo.alter_table_charter_sheets.sql
+++ migrations/008.undo.alter_table_charter_sheets.sql
@@ -1,2 +1,20 @@
 ALTER TABLE character_sheet
 ALTER COLUMN attack_and_spellcasting_info TEXT NOT NULL;
+DROP COLUMN acrobatics_expertise,
+DROP COLUMN animal_handling_expertise,
+DROP COLUMN arcana_expertise,
+DROP COLUMN athletics_expertise,
+DROP COLUMN deception_expertise,
+DROP COLUMN history_expertise,
+DROP COLUMN insight_expertise,
+DROP COLUMN intimidation_expertise,
+DROP COLUMN investigation_expertise,
+DROP COLUMN medicine_expertise,
+DROP COLUMN nature_expertise,
+DROP COLUMN perception_expertise,
+DROP COLUMN performance_expertise,
+DROP COLUMN persuasion_expertise,
+DROP COLUMN religon_expertise,
+DROP COLUMN sleight_of_hand_expertise,
+DROP COLUMN stealth_expertise,
+DROP COLUMN survival_expertise;
